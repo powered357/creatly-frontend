0f781ff91ea36d133af14fc94b5eb5bc069b9075
Jenia Enakin <fend3rock@gmail.com>
2021-02-17 23:32:32 +0200

    Setup tests

---
+++ src/components/App.test.js
@@ -0,0 +1,16 @@
+import { screen, render, cleanup } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+  beforeAll(() => {
+    render(<App />);
+  });
+
+  it('should have the right message in the dom', () => {
+    const message = 'Courses Platform';
+
+    expect(screen.getByText(message)).toBeInTheDocument();
+  });
+
+  afterAll(cleanup);
+});
