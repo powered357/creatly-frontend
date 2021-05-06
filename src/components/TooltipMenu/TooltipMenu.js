4b17caa52ae2b67d8454609290434a10ee92d648
Nikita Gorchakov <nikitagorchakov880091@gmail.com>
2021-05-06 12:03:45 +0500

    fix

--- src/components/TooltipMenu/TooltipMenu.js
+++ src/components/TooltipMenu/TooltipMenu.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
