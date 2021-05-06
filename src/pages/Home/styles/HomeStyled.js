a0646d5b3ae9da3abe6701110619dc1b8034bd8b
Jenia Enakin <fend3rock@gmail.com>
2021-05-06 18:37:17 +0300

    Added empty text when no courses available

--- src/pages/Home/styles/HomeStyled.js
+++ src/pages/Home/styles/HomeStyled.js
@@ -3,14 +3,23 @@ import styled from 'styled-components';
 import { Text } from 'UI-KIT';
 
 export const HomeStyled = styled.main`
-  padding-top: 25px;
+  padding: 50px 0 100px;
 `;
 
 export const Title = styled(Text).attrs(() => ({
   font: 'h1',
 }))`
   display: block;
-  padding: 25px;
+  padding: 0 25px;
+  margin-bottom: 25px;
 `;
 
 export const Content = styled.div``;
+
+export const EmptyText = styled(Text).attrs(() => ({
+  font: 't1',
+}))`
+  display: block;
+  max-width: 450px;
+  padding: 0 25px;
+`;
