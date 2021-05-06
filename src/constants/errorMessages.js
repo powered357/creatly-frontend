2fdfbc30c48b09b7dbe54f76f42fca0ec06cf012
Katrin <alimova.katrin8@gmail.com>
2021-05-06 09:55:42 +0300

    error message for existing mail

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,5 +1,5 @@
 export const ERROR_MESSAGES = {
   "user doesn't exists": 'Пользователя с таким email или паролем не существует',
   'user with such email already exists':
-    'Пользователь с таким email уже зарегистрирован. Если возникли проблемы, напиши мне на почту zhashkevychmaksim@gmail.com',
+    'Пользователь с таким email уже зарегистрирован. Если возникли проблемы, напиши мне на zhashkevychmaksim@gmail.com',
 };
